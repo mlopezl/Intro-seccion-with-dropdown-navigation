01cf9d5c6a152c05b2d56fb7e19090ddb1ac603e
maynor-lopez <maynor94l39@gmail.com>
2026-03-09 20:15:23 -0600

    Adding overlay and styles to the tablet version

--- app.js
+++ app.js
@@ -7,31 +7,46 @@ const featureArrow = document.getElementById('features-arrow');
 const companyArrow = document.getElementById('company-arrow');
 const featureMenu = document.getElementById('features-menu');
 const companyMenu = document.getElementById('company-menu');
+const body = document.getElementById('body');
 
 hamburgerIcon.addEventListener('click', () =>{
     menuMobile.classList.remove('hidden');
+    body.classList.add('overlay-activo');
 });
 
 closeIcon.addEventListener('click', () =>{
     menuMobile.classList.add('hidden');
+    body.classList.remove('overlay-activo');
 });
 
 features.addEventListener('click', () =>{
-   if(featureMenu.style.display === 'none'){
-    featureMenu.style.display = 'flex';
+   if(!featureMenu.classList.contains('hidden')){
+    featureMenu.classList.add('hidden');
     featureArrow.src = './images/icon-arrow-up.svg';
    } else{
-    featureMenu.style.display = 'none';
+    featureMenu.classList.remove('hidden');
     featureArrow.src = './images/icon-arrow-down.svg';
    }
 });
 
+
 company.addEventListener('click', () =>{
-   if(companyMenu.style.display === 'none'){
-    companyMenu.style.display = 'flex';
+   if(!companyMenu.classList.contains('hidden')){
+    companyMenu.classList.add('hidden');
     companyArrow.src = './images/icon-arrow-up.svg';
    } else{
-    companyMenu.style.display = 'none';
+    companyMenu.classList.remove('hidden');
     companyArrow.src = './images/icon-arrow-down.svg';
    }
-})
+});
+
+document.addEventListener("click", (e) => {
+  if (
+    !closeIcon.contains(e.target) &&
+    !menuMobile.contains(e.target) &&
+    !hamburgerIcon.contains(e.target)
+  ) {
+    menuMobile.classList.add("hidden");
+    body.classList.remove("overlay-activo");
+  }
+});
